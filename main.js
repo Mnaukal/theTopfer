ab2e2b4f007c7a76004806900e7864c6e718c530
Mnaukal <michal.topfer@gmail.com>
2016-09-22 15:38:45 +0200

    simple slider

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+var moveTime = 5;
+var timer = 0;
+var countTimer = true;
+var itemCount = 0;
+var current = 0;
+
 $(document).ready(function() {
+    itemCount = $("#innerSlider").children(".slideritem").length;
+    var innerWidth = $(document).innerWidth();
+    $("#innerSlider").css("width", innerWidth * itemCount);
     
+    
+    setInterval(function() {
+        if(countTimer) {
+            timer++;
+            $("#page").text(timer);
+        }
+        
+        if(timer > moveTime) {
+            var last = current;
+            current++;
+            if(current >= itemCount)
+                current = 0;
+            
+            timer = 0;
+            
+            $("#innerSlider").css("left", current * -100 + "%");
+        }
+    }, 1000);    
+    
+    $("#slider").mouseenter(function() {
+        countTimer = false;
+    });
+    $("#slider").mouseleave(function() {
+        countTimer = true;
+    });
 });
